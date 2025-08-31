06d21525d0c1766e5f1313caa897f96af472fe95
Anson <AnsonChung3@gmail.com>
2025-08-31 19:32:17 +0100

    Keeping bare bone project. Trim away most css styling. To be add back later

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ button.addEventListener("click", () => {
   alert('alert text');
   console.log('print in console');
 });
+
+const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+const list = document.getElementById("testList");
+
+for (let i = 0; i < fruits.length; i++) {
+  const li = document.createElement("li");
+  li.textContent = fruits[i];
+  list.appendChild(li);
+}
