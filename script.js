cdd6f59e795ac802a13ce9ebb7dfc4f3c8bad5f5
Anson Chung <ansonchung3@gmail.com>
2026-03-20 12:57:33 +0000

    Auto formatter on save

--- script.js
+++ script.js
@@ -65,7 +65,10 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 
 	document.querySelector('.nav-label').addEventListener('click', () => {
-		const firstSubItem = document.querySelector('.nav-label').closest('.nav-group').querySelector('.sub-nav [data-target]');
+		const firstSubItem = document
+			.querySelector('.nav-label')
+			.closest('.nav-group')
+			.querySelector('.sub-nav [data-target]');
 		showSection(firstSubItem.dataset.target);
 	});
 
